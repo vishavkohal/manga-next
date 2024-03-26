64a441ba8e280f920431767164668d4670eed2c8
VISHAV <vishavkohal@gmail.com>
2024-03-26 15:52:50 +0530

    add readmore option

--- pages/Chapter.js
+++ pages/Chapter.js
@@ -4,16 +4,22 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { Card, CardContent, Typography, Grid, Container,Box,CircularProgress } from '@mui/material';
-import { CardMedia, Chip,FormControl, InputLabel, Select,MenuItem } from '@mui/material';
+import { CardMedia, Chip,FormControl, InputLabel, Select,MenuItem, Button,useMediaQuery, useTheme } from '@mui/material';
 import '../app/globals.css';
 import Navbar from './data/NavBar'
 export default function MangaInfo() {
+  const [showMoreDetails, setShowMoreDetails] = useState(false);
   const [mangaDetails, setMangaDetails] = useState(null);
   const [selectedChapter, setSelectedChapter] = useState(''); // F
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
     const router = useRouter();
     const { chapterId } = router.query;
+    const [isReadMore, setIsReadMore] = useState(true);
+  
+    // Hook to check for screen size, to show/hide additional information automatically
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   useEffect(() => {
     const fetchMangaDetails = async () => {
@@ -28,6 +34,10 @@ export default function MangaInfo() {
     fetchMangaDetails();
   }, [chapterId]);
 
+  const toggleDetails = () => {
+    setShowMoreDetails(!showMoreDetails);
+  };
+  
   const handleChapterChange = (event) => {
     const chapterId = event.target.value;
     setSelectedChapter(chapterId);
@@ -47,7 +57,9 @@ export default function MangaInfo() {
     });
   };
 
-
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
 
   if (!mangaDetails) {
     return (
@@ -79,6 +91,8 @@ export default function MangaInfo() {
     <Typography variant="body1">Alternative Titles: {mangaDetails.alternative.join(', ')}</Typography>
     <Typography variant="body1">Authors: {mangaDetails.authors.join(', ')}</Typography>
     <Typography variant="body1">Status: {mangaDetails.status}</Typography>
+    {(!isReadMore || !isSmallScreen) && (
+            <>
     <Typography variant="body1">Last Updated: {mangaDetails.lastUpdate}</Typography>
     <Typography variant="body1">Views: {mangaDetails.view}</Typography>
     <Typography variant="body1">Rating: {mangaDetails.rate}</Typography>
@@ -88,14 +102,24 @@ export default function MangaInfo() {
     <Grid container spacing={1}>
         {mangaDetails.genres.map((genre, index) => (
             <Grid item key={index}>
-                <Chip label={genre} />
+                <Chip label={genre} variant="outlined" sx={{
+                margin: 0.5,
+                borderColor: 'white', 
+                '& .MuiChip-label': {color: 'white'},// Darker border
+                }}/>
             </Grid>
         ))}
+      
     </Grid>
+    </>
+          )}
 </Grid>
 
       </Grid>
-
+      {(isSmallScreen) &&
+      <Button onClick={toggleReadMore} style={{ color: 'white', border: '1px solid #ffffff', marginTop: '15px' }}>
+            {isReadMore ? 'Read More' : 'Show Less'}
+          </Button>}
       <Grid container spacing={2} alignItems="center" className="pt-8">
         {/* Chapter Heading */}
         <Grid item xs={12} sm={6}>
@@ -116,8 +140,9 @@ export default function MangaInfo() {
               label="Chapter"
               style={{
                 color: '#ffffff',  // Text color for better visibility
-                backgroundColor: '#424242',  // A darker background that contrasts with the text color
-                border: '1px solid #ffffff', // Optional: adds a border that makes the element stand out
+                backgroundColor: 'black',  // A darker background that contrasts with the text color
+                border: '1px solid #ffffff',
+                borderRadius:'20px' // Optional: adds a border that makes the element stand out
               }}
             >
               {mangaDetails.chapters.map((chapter, index) => (
@@ -133,17 +158,28 @@ export default function MangaInfo() {
       <Grid container spacing={3}>
         {mangaDetails.chapters.map((chapter, index) => (
           <Grid item xs={12} sm={6} md={4} key={index} className="hover:scale-105 transition-transform duration-200">
-            <Card raised onClick={() => handleClick(chapter.chapterID)}  className="cursor-pointer">
+            <Card raised onClick={() => handleClick(chapter.chapterID)}  className="cursor-pointer" sx={{borderRadius:'30px', backgroundColor:'black','&:hover': {
+                  transform: 'scale(1.1)', // Zoom effect on hover
+                  transition: 'transform 0.3s', // Smooth transition
+                }}}>
               <CardContent>
-                <Typography variant="h6" component="h3">
-                  {chapter.chapterName}
-                </Typography>
-                <Typography color="textSecondary">
-                  Views: {chapter.views}
-                </Typography>
-                <Typography color="textSecondary">
-                  Uploaded: {chapter.timeUploaded}
-                </Typography>
+              <Typography variant="h6" component="h2" color="white">
+                    Chapter {mangaDetails.chapters.length - index}
+                  </Typography>
+              <Chip label={`Views: ${chapter.views}`  }
+              variant="outlined"
+              sx={{
+                margin: 0.5,
+                borderColor: 'white', 
+                '& .MuiChip-label': {color: 'white'},// Darker border
+                }}
+                />
+                  <Chip label={`Uploaded: ${chapter.timeUploaded}`}  variant="outlined"
+              sx={{
+                margin: 0.5,
+                borderColor: 'white', 
+                '& .MuiChip-label': {color: 'white'},// Darker border
+                }}/>
               </CardContent>
             </Card>
           </Grid>
